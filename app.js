9bd76879cc6f8c9639a9d93073b9324580acd549
KamataRyo <mugil.cephalus+github@gmail.com>
2016-04-18 15:16:26 +0900

    Add i18n

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 var ENDPOINT = 'http://api.c4w.jp/api/v1/pages.json';
 
-var app = angular.module('bss', []);
+var app = angular.module('bss', ['pascalprecht.translate']);
+
+app.config([
+    '$translateProvider',
+    function($translateProvider){
+
+        $translateProvider.useStaticFilesLoader({
+            prefix: 'assets/language/lang-',
+            suffix: '.json'
+        });
+
+        $translateProvider.useSanitizeValueStrategy(null);
+        $translateProvider.preferredLanguage('ja');
+    }
+]);
 
 
-app.controller('contentCtrl',[
-    '$http',
+app.controller('consoleCtrl', [
     '$scope',
-    function($http, $scope){
+    '$translate',
+    function($scope, $translate){
+        $scope.changeLanguage = function(key){
+            $translate.use(key);
+        }
+    }
+]);
+
+
+
+app.controller('contentCtrl', [
+    '$http',
+    '$translate',
+    '$scope',
+    function($http, $translate, $scope){
 
         $http({
             Method: 'GET',
@@ -27,18 +54,19 @@ app.controller('contentCtrl',[
                     if (delta_msec < 1000) {//今
                         dateTimeDiff = '今';
                     } else if (delta_msec < 1000 * 60) {//x秒前
-                        dateTimeDiff = parseInt(delta_msec / 1000) + '秒前';
+                        dateTimeDiff = parseInt(delta_msec / 1000) + $translate('SEC_LATER');
                     } else if (delta_msec < 1000 * 60 * 60) {
                         //x分前
                         dateTimeDiff = parseInt(delta_msec / (1000 * 60)) + '分前';
                     } else if (delta_msec < 1000 * 60 * 60 * 24) {
                         //x時間前
-                        dateTimeDiff = parseInt(delta_msec / (1000 * 60 * 60)) + '時間前';
+                        dateTimeDiff = parseInt(delta_msec / (1000 * 60 * 60)) + $translate('AFTER_POST.HOUR');
                     } else {
                         //x日前
                         dateTimeDiff = parseInt(delta_msec / (1000 * 60 * 60 * 60)) + '日前';
                     }
 
+                    console.log($translate('AFTER_POST.HOUR'));
                     entry.date = new Date(rawDate).toLocaleDateString();
                     entry.time = new Date(rawDate).toLocaleTimeString();
                     entry.dateTimeDiff = dateTimeDiff;
